76fbf4d9f326759ac1851c53b9f358618d7b29bd
ricksonolveira <ricksonoliveira>
2021-12-25 22:41:59 -0300

    feat: adding user_currency tests

--- test/currency_converter/accounts_test.exs
+++ test/currency_converter/accounts_test.exs
@@ -2,7 +2,7 @@ defmodule CurrencyConverter.AccountsTest do
   use CurrencyConverter.DataCase
 
   alias CurrencyConverter.Accounts
-  alias CurrencyConverter.Accounts.User
+  alias CurrencyConverter.Accounts.{User, UserCurrency}
   import CurrencyConverter.AccountsFixtures
 
   describe "users" do
@@ -33,6 +33,25 @@ defmodule CurrencyConverter.AccountsTest do
 
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+  end
+
+  describe "user_currency" do
+    test "create_user_currency/1 with valid data creates user_currency" do
+      valid_attrs = %{
+        conversion_rate: 1.00,
+        transaction_id: Ecto.UUID.generate(),
+        value_given: Decimal.new(100),
+        currency_origin: "USD",
+        currency_destiny: "BRL",
+        value_converted: Decimal.new(500),
+      }
+
+      assert {:ok, %UserCurrency{} = user_currency} = Accounts.create_user_currency(valid_attrs)
+    end
+
+    test "create_user_currency/1 with invalid data returns error" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user_currency(%{transaction_id: nil})
     end
   end
 end
